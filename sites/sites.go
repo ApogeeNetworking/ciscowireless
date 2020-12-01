c77b728259bfb6d0abc1f3bdcfd8c2f6f756992a
drkchiloll <sam.womack@entropicos.com>
2020-12-01 11:59:38 -0600

    work on AP Join Profiles

--- sites/sites.go
+++ sites/sites.go
@@ -2,48 +2,23 @@ package sites
 
 import (
 	"encoding/json"
-	"fmt"
+	"reflect"
 
 	"github.com/ApogeeNetworking/ciscowireless/requests"
 )
 
 // Service ...
 type Service struct {
-	http *requests.Service
+	http         *requests.Service
+	jsonContains func(keys []string, val string) bool
 }
 
 // NewService ...
-func NewService(req *requests.Service) *Service {
-	return &Service{http: req}
-}
-
-// GetApJoinProfiles ...
-func (s *Service) GetApJoinProfiles() ([]ApJoinProfileSummary, error) {
-	uri := "/Cisco-IOS-XE-wireless-site-cfg:site-cfg-data/ap-cfg-profiles/ap-cfg-profile"
-	req, err := s.http.GenerateRequest(uri, "GET", nil)
-	if err != nil {
-		return nil, err
+func NewService(req *requests.Service, contains func(keys []string, val string) bool) *Service {
+	return &Service{
+		http:         req,
+		jsonContains: contains,
 	}
-	res, err := s.http.MakeRequest(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	type Resp struct {
-		Response []ApJoinProfileSummary `json:"Cisco-IOS-XE-wireless-site-cfg:ap-cfg-profile"`
-	}
-	var cResp Resp
-	err = json.NewDecoder(res.Body).Decode(&cResp)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return cResp.Response, nil
-}
-
-// Tag ...
-type Tag struct {
-	Name          string `json:"site-tag-name"`
-	ApJoinProfile string `json:"ap-join-profile"`
 }
 
 // GetTags ...
@@ -61,10 +36,437 @@ func (s *Service) GetTags() ([]Tag, error) {
 		return nil, err
 	}
 	defer res.Body.Close()
-	type resp struct {
-		Response []Tag `json:"Cisco-IOS-XE-wireless-site-cfg:site-tag-config"`
+	var tagsMap map[string][]interface{}
+	json.NewDecoder(res.Body).Decode(&tagsMap)
+	tags := tagsMap["Cisco-IOS-XE-wireless-site-cfg:site-tag-config"]
+	var siteTags []Tag
+	for _, tagMap := range tags {
+		v := reflect.ValueOf(tagMap)
+		if v.Kind() == reflect.Map {
+			var siteTag Tag
+			var keys []string
+			for _, k := range v.MapKeys() {
+				key := k.String()
+				keys = append(keys, key)
+				switch key {
+				case "site-tag-name":
+					val := (v.MapIndex(k)).Interface().(string)
+					siteTag.Name = val
+				case "description":
+					val := (v.MapIndex(k)).Interface().(string)
+					siteTag.Description = val
+				case "ap-join-profile":
+					val := (v.MapIndex(k)).Interface().(string)
+					siteTag.ApJoinProfile = val
+				case "is-local-site":
+					val := (v.MapIndex(k)).Interface().(bool)
+					siteTag.IsLocalSite = val
+				case "flex-profile":
+					val := (v.MapIndex(k)).Interface().(string)
+					siteTag.FlexProfile = val
+				case "fabric-control-plane-name":
+					val := (v.MapIndex(k)).Interface().(string)
+					siteTag.FabricCntrlPlaneName = val
+				case "image-download-profile-name":
+					val := (v.MapIndex(k)).Interface().(string)
+					siteTag.ImageDownloadProfileName = val
+				case "arp-caching":
+					val := (v.MapIndex(k)).Interface().(bool)
+					siteTag.ArpCaching = val
+				}
+			}
+			isLocalSite := s.jsonContains(keys, "is-local-site")
+			if !isLocalSite {
+				siteTag.IsLocalSite = true
+			}
+			if !s.jsonContains(keys, "arp-caching") {
+				siteTag.ArpCaching = true
+			}
+			if !s.jsonContains(keys, "flex-profile") && isLocalSite {
+				siteTag.FlexProfile = "default-flex-profile"
+			}
+			siteTags = append(siteTags, siteTag)
+		}
 	}
-	var cResp resp
-	json.NewDecoder(res.Body).Decode(&cResp)
-	return cResp.Response, nil
+	return siteTags, err
+}
+
+// GetApJoinProfiles ...
+func (s *Service) GetApJoinProfiles() ([]ApJoinProfile, error) {
+	uri := "/Cisco-IOS-XE-wireless-site-cfg:site-cfg-data/ap-cfg-profiles/ap-cfg-profile"
+	req, err := s.http.GenerateRequest(uri, "GET", nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := s.http.MakeRequest(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var apJoinProfiles []ApJoinProfile
+	var profilesMap map[string][]interface{}
+	json.NewDecoder(res.Body).Decode(&profilesMap)
+	profiles := profilesMap["Cisco-IOS-XE-wireless-site-cfg:ap-cfg-profile"]
+	for _, profileMap := range profiles {
+		profile := s.handleJoinProfiles(profileMap)
+		apJoinProfiles = append(apJoinProfiles, profile)
+	}
+	return apJoinProfiles, nil
+}
+
+func (s *Service) handleJoinProfiles(m interface{}) ApJoinProfile {
+	var profile ApJoinProfile
+	v := reflect.ValueOf(m)
+	if v.Kind() == reflect.Map {
+		var keys []string
+		for _, k := range v.MapKeys() {
+			key := k.String()
+			keys = append(keys, key)
+			val := v.MapIndex(k).Interface()
+			switch key {
+			case "profile-name":
+				profile.Name = val.(string)
+			case "description":
+				profile.Description = val.(string)
+			// Default Value for this is FALSE
+			// If found in RESP then the value is TRUE
+			case "data-encryption-flag":
+				profile.DataEncryptFlag = val.(bool)
+			case "ap-packet-capture-profile":
+				profile.ApPktCapProfile = val.(string)
+			case "ap-trace-profile":
+				profile.ApTraceProfile = val.(string)
+			case "tcp-adjust-mss":
+				tam := val.(map[string]interface{})
+				for tamKey, tamVal := range tam {
+					switch tamKey {
+					case "adjust-mss":
+						profile.TCPMSS.Adjust = tamVal.(bool)
+					case "tcp-adjust-mss":
+						profile.TCPMSS.AdjustMSS = tamVal.(uint16)
+					}
+				}
+			case "led-state":
+				ls := val.(map[string]interface{})
+				for lsKey, lsVal := range ls {
+					if lsKey == "led-state" {
+						profile.LEDState.State = lsVal.(bool)
+					}
+				}
+			case "link-latency":
+				ll := val.(map[string]interface{})
+				for llKey, llVal := range ll {
+					if llKey == "link-latency-flag" {
+						profile.LinkLatency.Flag = llVal.(string)
+					}
+				}
+			case "device-mgmt":
+				dm := val.(map[string]interface{})
+				for dmKey, dmVal := range dm {
+					switch dmKey {
+					case "ssh":
+						profile.DeviceMgmt.SSH = dmVal.(bool)
+					case "telnet":
+						profile.DeviceMgmt.Telnet = dmVal.(bool)
+					}
+				}
+			case "user-mgmt":
+				um := val.(map[string]interface{})
+				var keys []string
+				for umKey, umVal := range um {
+					keys = append(keys, umKey)
+					val := umVal.(string)
+					switch umKey {
+					case "username":
+						profile.UserMgmt.Username = val
+					case "password":
+						profile.UserMgmt.Password = val
+					case "password-type":
+						profile.UserMgmt.PasswordType = umVal.(CrypType)
+					case "secret":
+						profile.UserMgmt.Secret = val
+					case "secret-type":
+						profile.UserMgmt.SecretType = umVal.(CrypType)
+					}
+				}
+				if !s.jsonContains(keys, "secret-type") {
+					profile.UserMgmt.SecretType = "clear"
+				}
+				if !s.jsonContains(keys, "password-type") {
+					profile.UserMgmt.PasswordType = "clear"
+				}
+			case "tunnel":
+				var keys []string
+				tun := val.(map[string]interface{})
+				for tunKey, tunVal := range tun {
+					keys = append(keys, tunKey)
+					val := tunVal.(string)
+					switch tunKey {
+					case "preferred-mode":
+						profile.CapWapTunnel.PreferredMode = val
+					case "udp-lite":
+						profile.CapWapTunnel.UDPLite = val
+					}
+				}
+				if !s.jsonContains(keys, "preferred-mode") {
+					profile.CapWapTunnel.PreferredMode = "preferred-mode-unconfig"
+				}
+				if !s.jsonContains(keys, "udp-lite") {
+					profile.CapWapTunnel.UDPLite = "udplite-checksum-disabled"
+				}
+			case "capwap-timer":
+				var keys []string
+				cwt := val.(map[string]interface{})
+				for cwtKey, cwtVal := range cwt {
+					keys = append(keys, cwtKey)
+					val := cwtVal.(uint8)
+					switch cwtKey {
+					case "heart-beat-timeout":
+						profile.CapWapTimeouts.HeartBeat = val
+					case "discovery-timeout":
+						profile.CapWapTimeouts.Discovery = val
+					case "fast-heart-beat-timeout":
+						profile.CapWapTimeouts.FastHeartBeat = val
+					case "primary-discovery-timeout":
+						profile.CapWapTimeouts.PrimaryDiscovery = val
+					case "primed-join-timeout":
+						profile.CapWapTimeouts.PrimedJoin = val
+					}
+				}
+				if !s.jsonContains(keys, "heart-beat-timeout") {
+					profile.CapWapTimeouts.HeartBeat = 30
+				}
+				if !s.jsonContains(keys, "discovery-timeout") {
+					profile.CapWapTimeouts.Discovery = 10
+				}
+				if !s.jsonContains(keys, "primery-discovery-timeout") {
+					profile.CapWapTimeouts.PrimaryDiscovery = 120
+				}
+			case "retransmit-timer":
+				var keys []string
+				rt := val.(map[string]interface{})
+				for rtKey, rtVal := range rt {
+					keys = append(keys, rtKey)
+					val := rtVal.(uint8)
+					if rtKey == "count" {
+						profile.RetransmitTimer.Count = val
+					}
+					if rtKey == "interval" {
+						profile.RetransmitTimer.Interval = val
+					}
+				}
+				if !s.jsonContains(keys, "count") {
+					profile.RetransmitTimer.Count = 5
+				}
+				if !s.jsonContains(keys, "interval") {
+					profile.RetransmitTimer.Interval = 3
+				}
+			case "login-credentials":
+				dlc := val.(map[string]interface{})
+				for dlcKey, dlcVal := range dlc {
+					val := dlcVal.(string)
+					switch dlcKey {
+					case "dot1x-username":
+						profile.Dot1xCreds.Username = val
+					case "dot1x-password":
+						profile.Dot1xCreds.Password = val
+					case "dot1x-password-type":
+						profile.Dot1xCreds.PasswordType = dlcVal.(CrypType)
+					}
+				}
+			case "dot1x-eap-type-info":
+				deti := val.(map[string]interface{})
+				for detiKey, detiVal := range deti {
+					val := detiVal.(string)
+					if detiKey == "dot1x-eap-type" {
+						profile.Dot1xEapInfo.Type = val
+					}
+				}
+				if profile.Dot1xEapInfo.Type == "" {
+					profile.Dot1xEapInfo.Type = "dot1x-eap-fast"
+				}
+			case "lsc-ap-auth-type-info":
+				lati := val.(map[string]interface{})
+				for latiKey, latiVal := range lati {
+					val := latiVal.(string)
+					if latiKey == "lsc-ap-auth-type" {
+						profile.LSCApAuthInfo.Type = val
+					}
+				}
+				if profile.LSCApAuthInfo.Type == "" {
+					profile.LSCApAuthInfo.Type = "lsc-ap-auth-capwap-dtls"
+				}
+			case "usb-module-status":
+				uv := val.(map[string]interface{})
+				for uKey, ukVal := range uv {
+					switch uKey {
+					case "enable":
+						profile.UsbStatus.Enabled = ukVal.(bool)
+					}
+				}
+			// Default Value: False
+			case "jumbo-mtu":
+				jm := val.(map[string]interface{})
+				for _, jmVal := range jm {
+					val := jmVal.(bool)
+					profile.JumboMTU.MTU = val
+				}
+			case "coredump":
+				cd := val.(map[string]interface{})
+				for cdKey, cdVal := range cd {
+					switch cdKey {
+					case "coredump-flag":
+						profile.CoreDump.Flag = cdVal.(MemoryCoreDumpProp)
+					case "tftp-server-address":
+						profile.CoreDump.TFTPServerAddr = cdVal.(string)
+					case "corefile-name":
+						profile.CoreDump.FileName = cdVal.(string)
+					}
+				}
+			case "syslog":
+				var keys []string
+				sl := val.(map[string]interface{})
+				for slKey, slVal := range sl {
+					keys = append(keys, slKey)
+					switch slKey {
+					case "facility-value":
+						profile.Syslog.FacValue = slVal.(SyslogFacProp)
+					case "log-level":
+						profile.Syslog.LogLevel = slVal.(SyslogLevelProp)
+					case "host":
+						profile.Syslog.Host = slVal.(string)
+					case "tls-mode":
+						profile.Syslog.TLSMode = slVal.(bool)
+					}
+				}
+				if !s.jsonContains(keys, "facility-value") {
+					profile.Syslog.FacValue = SyslogFacility.Kern
+				}
+				if !s.jsonContains(keys, "log-level") {
+					profile.Syslog.LogLevel = SyslogLevel.Info
+				}
+				if !s.jsonContains(keys, "host") {
+					profile.Syslog.Host = "255.255.255.255"
+				}
+			case "backup-controller":
+				bc := val.(map[string]interface{})
+				for bcKey, bcVal := range bc {
+					switch bcKey {
+					case "fallback-enabled":
+						profile.BackupControllers.Enabled = bcVal.(bool)
+					case "primary-controller-name":
+						profile.BackupControllers.PrimaryName = bcVal.(string)
+					case "secondary-controller-name":
+						profile.BackupControllers.SecondaryName = bcVal.(string)
+					case "primary-controller-ip":
+						profile.BackupControllers.PrimaryIPAddr = bcVal.(string)
+					case "secondary-controller-ip":
+						profile.BackupControllers.SecondaryIPAddr = bcVal.(string)
+					}
+				}
+			case "hyperlocation":
+			case "rogue-detection":
+			case "lag-info":
+			case "tftp-down-grade":
+			case "traffic-limit":
+			case "cdp":
+				c := val.(map[string]interface{})
+				for cKey, cVal := range c {
+					if cKey == "cdp-enable" {
+						profile.Cdp.Enabled = cVal.(bool)
+					}
+				}
+			case "capwap-window":
+			case "mesh":
+			case "ntp-server":
+				profile.NTPServer = val.(string)
+			case "reporting-interval":
+			case "ext-module":
+			case "icap":
+			case "lawful-interception":
+			case "persistent-ssid-broadcast":
+			case "dhcp-server":
+			case "halo-ble-entries":
+			case "icap-full-packet-trace-client-mac-address-entries":
+			case "icap-partial-packet-trace-client-mac-address-entries":
+			case "icap-anomaly-detection-client-mac-address-entries":
+			case "icap-client-statistics-client-mac-address-entries":
+			case "gas-rate-limit":
+			case "qosmap":
+			case "qosmap-dscp-to-ups":
+			case "qosmap-dscp-to-up-exceptions":
+			case "client-rssi":
+			case "ntp-server-info":
+			case "awips-enabled":
+			case "accounting":
+			case "apphost":
+			case "aux-client-interface":
+			case "proxy":
+			case "grpc-enable":
+			case "bssid-enable-stats":
+			case "bssid-stats-frequency":
+			case "priviate-ip-discovery":
+			case "public-ip-discovery":
+			case "led-flash":
+			case "bssid-neighbor-stats-enable":
+			case "bssid-neighbor-stats-frequency":
+			case "traffic-distribution":
+			case "dhcp-fallback":
+			}
+		}
+		// Fill Default Values for JSON Keys
+		if !s.jsonContains(keys, "backup-controllers") {
+			profile.BackupControllers.Enabled = true
+		}
+		if !s.jsonContains(keys, "ntp-server") {
+			profile.NTPServer = "0.0.0.0"
+		}
+		if !s.jsonContains(keys, "cdp") {
+			profile.Cdp.Enabled = true
+		}
+		if !s.jsonContains(keys, "syslog") {
+			profile.Syslog.FacValue = SyslogFacility.Kern
+			profile.Syslog.LogLevel = SyslogLevel.Info
+			profile.Syslog.Host = "255.255.255.255"
+		}
+		if !s.jsonContains(keys, "coredump") {
+			profile.CoreDump.Flag = MemCoreDump.Disable
+		}
+		if !s.jsonContains(keys, "tcp-adjust-mss") {
+			profile.TCPMSS.Adjust = true
+			profile.TCPMSS.AdjustMSS = 1250
+		}
+		if !s.jsonContains(keys, "led-state") {
+			profile.LEDState.State = true
+		}
+		if !s.jsonContains(keys, "tunnel") {
+			profile.CapWapTunnel.PreferredMode = "preferred-mode-unconfig"
+			profile.CapWapTunnel.UDPLite = "udplite-checksum-disabled"
+		}
+		if !s.jsonContains(keys, "capwap-timer") {
+			profile.CapWapTimeouts.HeartBeat = 30
+			profile.CapWapTimeouts.PrimaryDiscovery = 120
+			profile.CapWapTimeouts.Discovery = 10
+		}
+		if !s.jsonContains(keys, "retransmit-timer") {
+			profile.RetransmitTimer.Count = 5
+			profile.RetransmitTimer.Interval = 3
+		}
+		if !s.jsonContains(keys, "login-credentials") {
+			profile.Dot1xCreds.PasswordType = CryptTypes.Clear
+		}
+		if !s.jsonContains(keys, "dot1x-eap-type-info") {
+			profile.Dot1xEapInfo.Type = "dot1x-eap-fast"
+		}
+		if !s.jsonContains(keys, "lsc-ap-auth-type-info") {
+			profile.LSCApAuthInfo.Type = "lsc-ap-auth-capwap-dtls"
+		}
+		if !s.jsonContains(keys, "link-latency") {
+			profile.LinkLatency.Flag = "link-auditing-disable"
+		}
+		if !s.jsonContains(keys, "usb-module-status") {
+			profile.UsbStatus.Enabled = true
+		}
+	}
+	return profile
 }
--- sites/types.go
+++ sites/types.go
@@ -20,38 +20,255 @@ type ApJoinProfile struct {
 	ApTraceProfile  string `json:"ap-trace-profile,omitempty"`
 	// DEFAULT: 180
 	StatsTimer struct {
-		Timer int32 `json:"stats-timer"`
+		Timer uint32 `json:"stats-timer"`
 	} `json:"stats-timer"`
+	// TCP Maximum Segement Size
+	TCPMSS struct {
+		// Default: TRUE
+		Adjust bool `json:"adjust-mss"`
+		// Default: 1250
+		AdjustMSS uint16 `json:"tcp-adjust-mss"`
+	} `json:"tcp-mss,omitempty"`
 	// DEFAULT: TRUE
 	LEDState struct {
 		State bool `json:"led-state"`
 	} `json:"led-state"`
-	// DEFAULT: link-auditing-disable
 	LinkLatency struct {
+		// DEFAULT: link-auditing-disable
 		Flag string `json:"link-latency-flag"`
 	} `json:"link-latency"`
-	// DEFAULT: FALSE
+	CapWapTunnel struct {
+		PreferredMode string `json:"preferred-mode"`
+		UDPLite       string `json:"udp-lite"`
+	} `json:"tunnel"`
+	CapWapTimeouts struct {
+		// Default: 30
+		HeartBeat uint8 `json:"heart-beat-timeout"`
+		// Default: 10
+		Discovery uint8 `json:"discovery-timeout"`
+		// Default: 0
+		FastHeartBeat uint8 `json:"fast-heart-beat-timeout"`
+		// Default: 120
+		PrimaryDiscovery uint8 `json:"primary-discovery-timeout"`
+		// Default: 0
+		PrimedJoin uint8 `json:"primed-join-timeout"`
+	} `json:"capwap-timer"`
+	RetransmitTimer struct {
+		// Default: 5
+		Count uint8 `json:"count"`
+		// Default: 3
+		Interval uint8 `json:"interval"`
+	} `json:"retransmit-timer"`
+	Dot1xCreds struct {
+		Username     string   `json:"dot1x-username"`
+		Password     string   `json:"dot1x-password"`
+		PasswordType CrypType `json:"dot1x-password-type"`
+	} `json:"login-credentials"`
+	Dot1xEapInfo struct {
+		Type string `json:"dot1x-eap-type"`
+	} `json:"dot1x-eap-type-info"`
+	LSCApAuthInfo struct {
+		Type string `json:"lsc-ap-auth-type"`
+	} `json:"lsc-ap-auth-type-info"`
 	JumboMTU struct {
+		// DEFAULT: FALSE
 		MTU bool `json:"jumbo-mtu"`
 	} `json:"jumbo-mtu"`
-	UsbStatus  ApUsbModuleStatus `json:"usb-module-status,omitempty"`
-	DeviceMgmt DeviceMgmtParams  `json:"device-mgmt,omitempty"`
-	UserMgmt   UserMgmtParams    `json:"user-mgmt,omitempty"`
+	UsbStatus  ApJoinProfileUsbModuleStatus  `json:"usb-module-status,omitempty"`
+	DeviceMgmt ApJoinProfileDeviceMgmtParams `json:"device-mgmt,omitempty"`
+	UserMgmt   ApJoinProfileUserMgmtParams   `json:"user-mgmt,omitempty"`
+	CoreDump   struct {
+		Flag           MemoryCoreDumpProp `json:"coredump-flag"`
+		TFTPServerAddr string             `json:"tftp-server-name"`
+		FileName       string             `json:"corefile-name"`
+	} `json:"coredump"`
+	Syslog struct {
+		FacValue SyslogFacProp   `json:"facility-value"`
+		LogLevel SyslogLevelProp `json:"log-level"`
+		Host     string          `json:"host"`
+		TLSMode  bool            `json:"tls-mode"`
+	} `json:"syslog"`
+	Cdp struct {
+		Enabled bool `json:"cdp-enable"`
+	} `json:"cdp"`
+	NTPServer         string `json:"ntp-server"`
+	BackupControllers struct {
+		Enabled         bool   `json:"fallback-enabled"`
+		PrimaryName     string `json:"primary-controller-name"`
+		SecondaryName   string `json:"secondary-controller-name"`
+		PrimaryIPAddr   string `json:"primary-controller-ip"`
+		SecondaryIPAddr string `json:"secondary-controller-ip"`
+	} `json:"backup-controllers"`
 }
 
-// ApUsbModuleStatus ...
-type ApUsbModuleStatus struct {
+// ApJoinProfileUsbModuleStatus ...
+type ApJoinProfileUsbModuleStatus struct {
 	Enabled bool `json:"enable"`
 }
 
-// DeviceMgmtParams ...
-type DeviceMgmtParams struct {
-	SSH bool `json:"ssh"`
+// ApJoinProfileDeviceMgmtParams ...
+type ApJoinProfileDeviceMgmtParams struct {
+	Telnet bool `json:"telnet"`
+	SSH    bool `json:"ssh"`
 }
 
-// UserMgmtParams ...
-type UserMgmtParams struct {
+// ApJoinProfileUserMgmtParams ...
+type ApJoinProfileUserMgmtParams struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
-	Secret   string `json:"secret"`
+	// Default: CLEAR
+	PasswordType CrypType `json:"password-type"`
+	Secret       string   `json:"secret"`
+	// Default: CLEAR
+	SecretType CrypType `json:"secret-type"`
+}
+
+// CrypType ...
+type CrypType string
+
+// CrypTypes ...
+type CrypTypes struct {
+	Clear         CrypType
+	SHA           CrypType
+	MD5           CrypType
+	IkeAES        CrypType
+	Type7         CrypType
+	AES           CrypType
+	ClearToSha    CrypType
+	ClearToMd5    CrypType
+	ClearToIkeAes CrypType
+	ClearToType7  CrypType
+	ClearToAes    CrypType
+}
+
+// MemoryCoreDumpProp ...
+type MemoryCoreDumpProp string
+
+// MemoryCoreDumpFlag ...
+type MemoryCoreDumpFlag struct {
+	Compress   MemoryCoreDumpProp
+	Uncompress MemoryCoreDumpProp
+	Disable    MemoryCoreDumpProp
+}
+
+// SyslogFacProp ...
+type SyslogFacProp string
+
+// SyslogFacEnum ...
+type SyslogFacEnum struct {
+	Kern   SyslogFacProp
+	User   SyslogFacProp
+	Mail   SyslogFacProp
+	Daemon SyslogFacProp
+	Auth   SyslogFacProp
+	Syslog SyslogFacProp
+	Lpr    SyslogFacProp
+	News   SyslogFacProp
+	UUCP   SyslogFacProp
+	Sys9   SyslogFacProp
+	Sys10  SyslogFacProp
+	Sys11  SyslogFacProp
+	Sys12  SyslogFacProp
+	Sys13  SyslogFacProp
+	Sys14  SyslogFacProp
+	Cron   SyslogFacProp
+	Local0 SyslogFacProp
+	Local1 SyslogFacProp
+	Local2 SyslogFacProp
+	Local3 SyslogFacProp
+	Local4 SyslogFacProp
+	Local5 SyslogFacProp
+	Local6 SyslogFacProp
+	Local7 SyslogFacProp
+}
+
+// SyslogLevelProp ...
+type SyslogLevelProp string
+
+// SyslogLevelEnum ...
+type SyslogLevelEnum struct {
+	Emergency    SyslogLevelProp
+	Alert        SyslogLevelProp
+	Critical     SyslogLevelProp
+	Errors       SyslogLevelProp
+	Warning      SyslogLevelProp
+	Notification SyslogLevelProp
+	Info         SyslogLevelProp
+	Debug        SyslogLevelProp
+}
+
+var (
+	// CryptTypes ...
+	CryptTypes = CrypTypes{
+		Clear:         "clear",
+		SHA:           "sha",
+		MD5:           "md5",
+		IkeAES:        "ike-aes",
+		Type7:         "type7",
+		AES:           "aes",
+		ClearToSha:    "clear-to-sha",
+		ClearToMd5:    "clear-to-md5",
+		ClearToIkeAes: "clear-to-ike-aes",
+		ClearToType7:  "clear-to-type7",
+		ClearToAes:    "clear-to-aes",
+	}
+	// MemCoreDump ...
+	MemCoreDump = MemoryCoreDumpFlag{
+		Disable:    "tftp-coredump-disable",
+		Compress:   "tftp-coredump-compress",
+		Uncompress: "tftp-coredump-uncompress",
+	}
+	// SyslogFacility ...
+	SyslogFacility = SyslogFacEnum{
+		Kern:   "facility-kern",
+		User:   "facility-user",
+		Mail:   "facility-mail",
+		Daemon: "facility-daemon",
+		Auth:   "facility-auth",
+		Syslog: "facility-syslog",
+		Lpr:    "facility-lpr",
+		News:   "facility-news",
+		UUCP:   "facility-uucp",
+		Sys9:   "facility-sys9",
+		Sys10:  "facility-sys10",
+		Sys11:  "facility-sys11",
+		Sys12:  "facility-sys12",
+		Sys13:  "facility-sys13",
+		Sys14:  "facility-sys14",
+		Cron:   "facility-cron",
+		Local0: "facility-local0",
+		Local1: "facility-local1",
+		Local2: "facility-local2",
+		Local3: "facility-local3",
+		Local4: "facility-local4",
+		Local5: "facility-local5",
+		Local6: "facility-local6",
+		Local7: "facility-local7",
+	}
+	// SyslogLevel ...
+	SyslogLevel = SyslogLevelEnum{
+		Emergency: "syslog-level-emergency",
+		Alert:     "syslog-level-alert",
+		Critical:  "syslog-level-critical",
+		Errors:    "syslog-level-errors",
+		Warning:   "syslog-level-warning",
+		Info:      "syslog-level-information",
+		Debug:     "syslog-level-debug",
+	}
+)
+
+// Tag ...
+type Tag struct {
+	Name        string `json:"site-tag-name"`
+	Description string `json:"description"`
+	// Only Enabled if Site IS NOT Local
+	FlexProfile   string `json:"flex-profile,omitempty"`
+	ApJoinProfile string `json:"ap-join-profile"`
+	// Default: TRUE
+	IsLocalSite          bool   `json:"is-local-site"`
+	FabricCntrlPlaneName string `json:"fabric-control-plane-name,omitempty"`
+	// Default: default
+	ImageDownloadProfileName string `json:"image-download-profile-name,omitempty"`
+	// Default: TRUE
+	ArpCaching bool `json:"arp-caching"`
 }
