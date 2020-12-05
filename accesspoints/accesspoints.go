7217de8c81df82ecc60c3129437bec98fa252d27
drkchiloll <sam.womack@entropicos.com>
2020-12-05 10:00:23 -0600

    reduce unecessary types and var declarations

--- accesspoints/accesspoints.go
+++ accesspoints/accesspoints.go
@@ -67,20 +67,19 @@ func (s *Service) GetOne(macAddr string) (Ap, error) {
 		return s.GetOne(macAddr)
 	}
 	defer res.Body.Close()
-	type resp struct {
+	resp := struct {
 		Response capWapResp `json:"Cisco-IOS-XE-wireless-access-point-oper:capwap-data"`
-	}
-	var cResp resp
-	json.NewDecoder(res.Body).Decode(&cResp)
+	}{}
+	json.NewDecoder(res.Body).Decode(&resp)
 	ap := Ap{
-		Name:    cResp.Response.Name,
-		MacAddr: cResp.Response.Detail.Info.Board.MacAddr,
-		Serial:  cResp.Response.Detail.Info.Board.Serial,
-		Model:   cResp.Response.Detail.Info.Model.Model,
+		Name:    resp.Response.Name,
+		MacAddr: resp.Response.Detail.Info.Board.MacAddr,
+		Serial:  resp.Response.Detail.Info.Board.Serial,
+		Model:   resp.Response.Detail.Info.Model.Model,
 		Tag: ApTag{
-			Policy: cResp.Response.TagInfo.Policy.Name,
-			Site:   cResp.Response.TagInfo.Site.Name,
-			Rf:     cResp.Response.TagInfo.Rf.Name,
+			Policy: resp.Response.TagInfo.Policy.Name,
+			Site:   resp.Response.TagInfo.Site.Name,
+			Rf:     resp.Response.TagInfo.Rf.Name,
 		},
 	}
 	return ap, nil
@@ -178,12 +177,11 @@ func (s *Service) GetTagsFromAp(macAddr string) (ApTagCfg, error) {
 		return ApTagCfg{}, fmt.Errorf("unable to make request: %s", err)
 	}
 	defer res.Body.Close()
-	type resp struct {
+	resp := struct {
 		TagResp ApTagCfg `json:"Cisco-IOS-XE-wireless-ap-cfg:ap-tag"`
-	}
-	var r resp
-	json.NewDecoder(res.Body).Decode(&r)
-	return r.TagResp, nil
+	}{}
+	json.NewDecoder(res.Body).Decode(&resp)
+	return resp.TagResp, nil
 }
 
 // UpdateApTagCfg ...
