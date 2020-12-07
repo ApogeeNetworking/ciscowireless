57a4e48d8e59599c1a16760b0c68f3e05c6557e3
drkchiloll <sam.womack@entropicos.com>
2020-12-07 14:49:37 -0600

    externalize some Types

--- ciscotypes/models.go
+++ ciscotypes/models.go
@@ -24,3 +24,41 @@ type ApTagCfg struct {
 	Site    string `json:"site-tag,omitempty"`
 	Rf      string `json:"rf-tag,omitempty"`
 }
+
+// ApCdp ...
+type ApCdp struct {
+	LocalIntf      string
+	RemoteSw       string
+	RemoteIntf     string
+	RemoteSwIPAddr string
+}
+
+// ApEthIntf ...
+type ApEthIntf struct {
+	Name   string
+	Status string
+	Speed  string
+	TxRcv  string
+	Drops  string
+}
+
+// ApLanPort ...
+type ApLanPort struct {
+	ID     int
+	Status LanPortState
+}
+
+// LanPortState string enable|disable
+type LanPortState string
+
+// LPState {Enable: LanPortState, Disable: LanPortState}
+type LPState struct {
+	Enable  LanPortState
+	Disable LanPortState
+}
+
+// ApLanPortState enum for ENABLE|DISABLE (do not offer other options)
+var ApLanPortState = LPState{
+	Enable:  "enable",
+	Disable: "disable",
+}
